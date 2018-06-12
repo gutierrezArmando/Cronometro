e38b5b6d4a604ef0964c2bd55941d65173e11319
Armando Gutiérrez <gutierrezjarr@gmail.com>
2018-06-12 11:43:12 -0800

    métodos básicos

--- app/src/main/java/com/example/armando/cronometro/StopwatchActivity.java
+++ app/src/main/java/com/example/armando/cronometro/StopwatchActivity.java
@@ -2,12 +2,42 @@ package com.example.armando.cronometro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class StopwatchActivity extends AppCompatActivity {
+
+    private int seconds = 0;
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+    }
+
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    public void onClickStop(View view) {
+        running = false;
+    }
+
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+        int hours = seconds/3600;
+        int minutes = (seconds%3600)/60;
+        int secs = seconds%60;
+        String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+        timeView.setText(time);
+        if(running) {
+            seconds++;
+        }
     }
 }
